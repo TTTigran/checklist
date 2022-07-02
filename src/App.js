1d106b3e48818a521e33881ba508dec0cd8a970a
TTTigran <martirosyantigran.developer@gmail.com>
2022-07-02 04:22:34 +0400

    restyle

--- src/App.js
+++ src/App.js
@@ -7,23 +7,16 @@ function App() {
     const [clote, setClote] = useState('');
     const [hygienic, setHygienic] = useState('')
     const [checked, setChecked] = useState([]);
-    const [hygienics, setHygienics] = useState(["soap","tooth brush","toothpaste"]);
+    const [hygienics, setHygienics] = useState(["soap", "tooth brush", "toothpaste"]);
     const [documents, setDocuments] = useState(["Passport", "covit-19 test"]);
-    const [clotes, setClotes] = useState(["T-shirt","Shoes"]);
-    let collection = [
-        {
-            tytle: "Hygienic",
-            data: hygienics
-        },
-        {
-            tytle: "Document",
-            data: documents
-        },
-        {
-            tytle: "Clotes",
-            data: clotes
-        },
-    ]
+    const [clotes, setClotes] = useState(["T-shirt", "Shoes"]);
+    let collection = [{
+        tytle: "Hygienic", data: hygienics
+    }, {
+        tytle: "Document", data: documents
+    }, {
+        tytle: "Clotes", data: clotes
+    },]
     const [list, setList] = useState(collection);
 
 
@@ -72,53 +65,58 @@ function App() {
         setHygienic(event.target.value);
     }
 
-    var isChecked = (item) =>
-        checked.includes(item) ? "checked-item" : "not-checked-item";
+    var isChecked = (item) => checked.includes(item) ? "checked-item" : "not-checked-item";
 
 
-    return (
-        <>
-            <div className="app">
-                <button onClick={() => addDocument(doc)}>+ Document</button>
-                <input name="document" onChange={handleChangeDoc}/>
-                <button onClick={() => addClote(clote)}>+ Clote</button>
-                <input name="clote" onChange={handleChangeClots}/>
-                <button onClick={() => addHygienic(hygienic)}>+ Hygienic</button>
-                <input name="hygienic" onChange={handleChangeHygienic}/>
+    return (<>
+        <div className="app">
+            <div className="title"><h1>Travel CheckList</h1></div>
 
-                <div className="title"><h1>Travel CheckList</h1></div>
-                <div className='sort-items'>
-                    {list && list.map((item, bigIndex) => (
-                        <div className="checkList">
-                            <b className='title-category'>{item.tytle}:</b>
-                            <div className="list-container">
-                                {item.data.map((item, index) => (
-                                    <div key={index}>
-                                        <input value={item} name={`checkbox${bigIndex}`} type="checkbox"
-                                               onChange={handleCheck}/>
-                                        <span className={isChecked(item)}>{item}</span>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                <div><h2>My Travel's Items:</h2></div>
-                <div>
-                    <ul>
-                        {checked && checked.map((todo, index) =>
-                            // Only do this if items have no stable IDs
-                            <li key={index}>
-                                {todo}
-                            </li>
-                        )}
-                    </ul>
-                </div>
+           <div className='adder-board'>
+               <div>
+                   <button onClick={() => addDocument(doc)}>+ Document</button>
+                   <input name="document" onChange={handleChangeDoc}/>
+               </div>
+
+               <div>
+                   <button onClick={() => addClote(clote)}>+ Clote</button>
+                   <input name="clote" onChange={handleChangeClots}/>
+               </div>
+
+               <div>
+                   <button onClick={() => addHygienic(hygienic)}>+ Hygienic</button>
+                   <input name="hygienic" onChange={handleChangeHygienic}/>
+               </div>
+           </div>
+
+
+
+
+            <div className='sort-items'>
+                {list && list.map((item, bigIndex) => (<div className="checkList">
+                    <b className='title-category'>{item.tytle}:</b>
+                    <div className="list-container">
+                        {item.data.map((item, index) => (<div key={index}>
+                            <input value={item} name={`checkbox${bigIndex}`} type="checkbox"
+                                   onChange={handleCheck}/>
+                            <span className={isChecked(item)}>{item}</span>
+                        </div>))}
+                    </div>
+                </div>))}
             </div>
+            <div><h2>My Travel's Items:</h2></div>
+            <div>
+                <ul>
+                    {checked && checked.map((todo, index) => // Only do this if items have no stable IDs
+                        <li key={index}>
+                            {todo}
+                        </li>)}
+                </ul>
+            </div>
+        </div>
 
 
-        </>
-    );
+    </>);
 
 }
 
